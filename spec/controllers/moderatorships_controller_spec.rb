2ed0ef05c9e30348e8d6efda235cd03e53948e73
rick <technoweenie@gmail.com>
2008-07-25 14:22:30 -0700

    fix moderatorships controller specs

--- spec/controllers/moderatorships_controller_spec.rb
+++ spec/controllers/moderatorships_controller_spec.rb
@@ -2,20 +2,13 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ModeratorshipsController, "POST #create" do
   before do
+    login_as :default
     @attributes = {'user_id' => users(:default).id.to_s, 'forum_id' => forums(:default).id.to_s}
-    @errors     = []
-    @errors.stub!(:full_messages).and_return(%w(foo bar))
-    @moderatorship = mock_model Moderatorship, :new_record? => false, :errors => @errors, :id => 5, :user => users(:default)
-    Moderatorship.stub!(:new).with(@attributes).and_return(@moderatorship)
   end
   
   describe ModeratorshipsController, "(successful creation)" do
     define_models
     act! { post :create, :moderatorship => @attributes }
-
-    before do
-      @moderatorship.stub!(:save).and_return(true)
-    end
     
     it_assigns :moderatorship, :flash => { :notice => :not_nil }
     it_redirects_to { user_path(users(:default)) }
@@ -23,11 +16,7 @@ describe ModeratorshipsController, "POST #create" do
 
   describe ModeratorshipsController, "(unsuccessful creation)" do
     define_models
-    act! { post :create, :moderatorship => @attributes }
-
-    before do
-      @moderatorship.stub!(:save).and_return(false)
-    end
+    act! { post :create, :moderatorship => @attributes.merge('forum_id' => '') }
     
     it_assigns :moderatorship, :flash => {:notice => nil, :error => :not_nil}
     it_redirects_to { user_path(users(:default)) }
@@ -36,37 +25,31 @@ describe ModeratorshipsController, "POST #create" do
   describe ModeratorshipsController, "(successful creation, xml)" do
     define_models
     act! { post :create, :moderatorship => @attributes, :format => 'xml' }
-
-    before do
-      @moderatorship.stub!(:save).and_return(true)
-      @moderatorship.stub!(:to_xml).and_return("mocked content")
-    end
     
-    it_assigns :moderatorship, :headers => { :Location => lambda { moderatorship_url(@moderatorship) } }
-    it_renders :xml, :moderatorship, :status => :created
+    it_assigns :moderatorship, :headers => { :Location => lambda { moderatorship_url(assigns(:moderatorship)) } }
+    it_renders :xml, :status => :created do
+      assigns(:moderatorship).to_xml
+    end
   end
   
   describe ModeratorshipsController, "(unsuccessful creation, xml)" do
     define_models
-    act! { post :create, :moderatorship => @attributes, :format => 'xml' }
-
-    before do
-      @moderatorship.stub!(:save).and_return(false)
-    end
+    act! { post :create, :moderatorship => @attributes.merge('forum_id' => ''), :format => 'xml' }
     
     it_assigns :moderatorship
-    it_renders :xml, "moderatorship.errors", :status => :unprocessable_entity
+    it_renders :xml, :status => :unprocessable_entity do
+      assigns(:moderatorship).errors.to_xml
+    end
   end
 end
 
 describe ModeratorshipsController, "DELETE #destroy" do
   define_models
-  act! { delete :destroy, :id => 1 }
+  act! { delete :destroy, :id => @moderatorship.to_param }
   
   before do
+    login_as :default
     @moderatorship = moderatorships(:default)
-    @moderatorship.stub!(:destroy)
-    Moderatorship.stub!(:find).with('1').and_return(@moderatorship)
   end
 
   it_assigns :moderatorship
@@ -74,7 +57,7 @@ describe ModeratorshipsController, "DELETE #destroy" do
   
   describe ModeratorshipsController, "(xml)" do
     define_models
-    act! { delete :destroy, :id => 1, :format => 'xml' }
+    act! { delete :destroy, :id => @moderatorship.to_param, :format => 'xml' }
 
     it_assigns :moderatorship
     it_renders :blank
