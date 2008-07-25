420a7f0086eb493a4bad3e85b3c707df7b9005db
rick <technoweenie@gmail.com>
2008-07-25 13:18:11 -0700

    fix forums controller specs

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -1,15 +1,13 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ForumsController, "GET #index" do
-  define_models :stubbed
+  define_models
 
   act! { get :index }
 
   before do
-    @site   = sites(:default)
-    @forums = [forums(:other), forums(:default)]
-    @site.stub!(:ordered_forums).and_return(@forums)
-    @controller.stub!(:current_site).and_return(@site)
+    login_as :default
+    current_site :default
     @controller.stub!(:admin_required).and_return(true)
     session[:forums_page] = {1 => 5}
     @forum_time = session[:forums] = {1 => 5.minutes.ago.utc}
@@ -19,29 +17,23 @@ describe ForumsController, "GET #index" do
   it_renders :template, :index
   
   describe ForumsController, "(xml)" do
-    define_models :stubbed
+    define_models
     
     act! { get :index, :format => 'xml' }
 
     it_assigns :forums
-    it_renders :xml, :forums
+    it_renders :xml
   end
 end
 
 describe ForumsController, "GET #show" do
-  define_models :stubbed
+  define_models
 
-  act! { get :show, :id => 1 }
+  act! { get :show, :id => @forum.to_param }
 
   before do
-    @site   = sites(:default)
+    current_site :default
     @forum  = forums(:default)
-    @topics = [topics(:default)]
-    @site.forums.stub!(:find_by_permalink).with('1').and_return(@forum)
-    @forum.topics.stub!(:paginate).with(:page => 1).and_return(@topics)
-    @controller.stub!(:current_site).and_return(@site)
-    @controller.stub!(:admin_required).and_return(true)
-    @controller.stub!(:logged_in?).and_return(false)
     @forum_page = session[:forums_page] = {@forum.id => 1}
     @forum_time = session[:forums]      = {@forum.id => Time.utc(2007, 1, 1)}
   end
@@ -50,38 +42,34 @@ describe ForumsController, "GET #show" do
   it_renders :template, :show
   
   it "sets session[:forums] if logged in" do
-    @controller.stub!(:logged_in?).and_return(true)
+    login_as :default
     act!
     session[:forums][@forum.id].should == current_time
   end
   
   describe ForumsController, "(paged)" do
-    define_models :stubbed
-    act! { get :show, :id => 1, :page => 5 }
-    before do
-      @forum.topics.stub!(:paginate).with(:page => 5).and_return(@topics)
-    end
+    define_models
+    act! { get :show, :id => @forum.to_param, :page => 5 }
     
     it_assigns :session => { :forums_page => lambda { {@forum.id => 5} } }
   end
   
   describe ForumsController, "(xml)" do
-    define_models :stubbed
+    define_models
     
-    act! { get :show, :id => 1, :format => 'xml' }
+    act! { get :show, :id => @forum.to_param, :format => 'xml' }
 
     it_assigns :topics => :undefined
-    it_renders :xml, :forum
+    it_renders :xml
   end
 end
 
 describe ForumsController, "GET #new" do
-  define_models :stubbed
+  define_models
   act! { get :new }
   before do
-    @site   = sites(:default)
-    @forum  = Forum.new
-    @controller.stub!(:current_site).and_return(@site)
+    login_as :default
+    current_site :default
     @controller.stub!(:admin_required).and_return(true)
   end
 
@@ -93,22 +81,21 @@ describe ForumsController, "GET #new" do
   it_renders :template, :new
   
   describe ForumsController, "(xml)" do
-    define_models :stubbed
+    define_models
     act! { get :new, :format => 'xml' }
 
-    it_renders :xml, :forum
+    it_renders :xml
   end
 end
 
 describe ForumsController, "GET #edit" do
-  define_models :stubbed
-  act! { get :edit, :id => 1 }
+  define_models
+  act! { get :edit, :id => @forum.to_param }
   
   before do
-    @site   = sites(:default)
-    @forum  = forums(:default)
-    @site.forums.stub!(:find_by_permalink).with('1').and_return(@forum)
-    @controller.stub!(:current_site).and_return(@site)
+    login_as :default
+    current_site :default
+    @forum  = forums(:default) 
     @controller.stub!(:admin_required).and_return(true)
   end
 
@@ -119,132 +106,94 @@ end
 describe ForumsController, "POST #create" do
   before do
     @attributes = {'name' => "Default"}
-    @forum      = mock_model Forum, :new_record? => false, :errors => []
-    @site       = sites(:default)
-    @site.forums.stub!(:build).with(@attributes).and_return(@forum)
-    @controller.stub!(:current_site).and_return(@site)
+    current_site :default
+    login_as :default
     @controller.stub!(:admin_required).and_return(true)
   end
   
   describe ForumsController, "(successful creation)" do
-    define_models :stubbed
+    define_models
     act! { post :create, :forum => @attributes }
-
-    before do
-      @forum.stub!(:save).and_return(true)
-    end
     
     it_assigns :forum, :flash => { :notice => :not_nil }
-    it_redirects_to { forum_path(@forum) }
+    it_redirects_to { forum_path(assigns(:forum)) }
   end
   
   describe ForumsController, "(successful creation, xml)" do
-    define_models :stubbed
+    define_models
     act! { post :create, :forum => @attributes, :format => 'xml' }
-
-    before do
-      @forum.stub!(:save).and_return(true)
-      @forum.stub!(:to_xml).and_return("<forum />")
-    end
     
-    it_assigns :forum, :headers => { :Location => lambda { forum_url(@forum) } }
-    it_renders :xml, :forum, :status => :created
+    it_assigns :forum, :headers => { :Location => lambda { forum_url(assigns(:forum)) } }
+    it_renders :xml, :status => :created
   end
 
   describe ForumsController, "(unsuccessful creation)" do
-    define_models :stubbed
-    act! { post :create, :forum => @attributes }
-
-    before do
-      @forum.stub!(:save).and_return(false)
-    end
+    define_models
+    act! { post :create, :forum => {:name => ''} }
     
     it_assigns :forum
     it_renders :template, :new
   end
   
   describe ForumsController, "(unsuccessful creation, xml)" do
-    define_models :stubbed
-    act! { post :create, :forum => @attributes, :format => 'xml' }
-
-    before do
-      @forum.stub!(:save).and_return(false)
-    end
+    define_models
+    act! { post :create, :forum => {:name => ''}, :format => 'xml' }
     
     it_assigns :forum
-    it_renders :xml, "forum.errors", :status => :unprocessable_entity
+    it_renders :xml, :status => :unprocessable_entity
   end
 end
 
 describe ForumsController, "PUT #update" do
   before do
+    login_as :default
+    current_site :default
     @attributes = {'name' => "Default"}
     @forum      = forums(:default)
-    @site       = sites(:default)
-    @site.forums.stub!(:find_by_permalink).with('1').and_return(@forum)
-    @controller.stub!(:current_site).and_return(@site)
     @controller.stub!(:admin_required).and_return(true)
   end
   
   describe ForumsController, "(successful save)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :forum => @attributes }
-
-    before do
-      @forum.stub!(:save).and_return(true)
-    end
+    define_models
+    act! { put :update, :id => @forum.to_param, :forum => @attributes }
     
     it_assigns :forum, :flash => { :notice => :not_nil }
     it_redirects_to { forum_path(@forum) }
   end
   
   describe ForumsController, "(successful save, xml)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :forum => @attributes, :format => 'xml' }
+    define_models
+    act! { put :update, :id => @forum.to_param, :forum => @attributes, :format => 'xml' }
 
-    before do
-      @forum.stub!(:save).and_return(true)
-    end
-    
     it_assigns :forum
     it_renders :blank
   end
 
   describe ForumsController, "(unsuccessful save)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :forum => @attributes }
+    define_models
+    act! { put :update, :id => @forum.to_param, :forum => {:name => ''} }
 
-    before do
-      @forum.stub!(:save).and_return(false)
-    end
-    
     it_assigns :forum
     it_renders :template, :edit
   end
   
   describe ForumsController, "(unsuccessful save, xml)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :forum => @attributes, :format => 'xml' }
+    define_models
+    act! { put :update, :id => @forum.to_param, :forum => {:name => ''}, :format => 'xml' }
 
-    before do
-      @forum.stub!(:save).and_return(false)
-    end
-    
     it_assigns :forum
-    it_renders :xml, "forum.errors", :status => :unprocessable_entity
+    it_renders :xml, :status => :unprocessable_entity
   end
 end
 
 describe ForumsController, "DELETE #destroy" do
-  define_models :stubbed
-  act! { delete :destroy, :id => 1 }
+  define_models
+  act! { delete :destroy, :id => @forum.to_param }
   
   before do
+    login_as :default
+    current_site :default
     @forum      = forums(:default)
-    @forum.stub!(:destroy)
-    @site       = sites(:default)
-    @site.forums.stub!(:find_by_permalink).with('1').and_return(@forum)
-    @controller.stub!(:current_site).and_return(@site)
     @controller.stub!(:admin_required).and_return(true)
   end
 
@@ -252,8 +201,8 @@ describe ForumsController, "DELETE #destroy" do
   it_redirects_to { forums_path }
   
   describe ForumsController, "(xml)" do
-    define_models :stubbed
-    act! { delete :destroy, :id => 1, :format => 'xml' }
+    define_models
+    act! { delete :destroy, :id => @forum.to_param, :format => 'xml' }
 
     it_assigns :forum
     it_renders :blank
