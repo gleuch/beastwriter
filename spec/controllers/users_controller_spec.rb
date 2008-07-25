b95167c6cdab681d3de7bd73c18dc03076251909
rick <technoweenie@gmail.com>
2008-07-25 14:41:16 -0700

    fix users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -86,14 +86,11 @@ describe UsersController, "PUT #update" do
     login_as :default
     current_site :default
     @attributes = {'login' => "Default"}
-    @controller.stub!(:current_site).and_return(@site)
-    @controller.stub!(:login_required).and_return(true)
-    @controller.stub!(:find_user)
   end
   
   describe UsersController, "(successful save)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :user => @attributes }
+    define_models
+    act! { put :update, :id => @user.to_param, :user => @attributes }
 
     before do
       @user.stub!(:save).and_return(true)
@@ -104,8 +101,8 @@ describe UsersController, "PUT #update" do
   end
   
   describe UsersController, "(successful save, xml)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :user => @attributes, :format => 'xml' }
+    define_models
+    act! { put :update, :id => @user.to_param, :user => @attributes, :format => 'xml' }
 
     before do
       @user.stub!(:save).and_return(true)
@@ -116,26 +113,20 @@ describe UsersController, "PUT #update" do
   end
 
   describe UsersController, "(unsuccessful save)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :user => @attributes }
-
-    before do
-      @user.stub!(:save).and_return(false)
-    end
+    define_models
+    act! { put :update, :id => @user.to_param, :user => {:email => ''} }
     
     it_assigns :user
     it_renders :template, :edit
   end
   
   describe UsersController, "(unsuccessful save, xml)" do
-    define_models :stubbed
-    act! { put :update, :id => 1, :user => @attributes, :format => 'xml' }
-
-    before do
-      @user.stub!(:save).and_return(false)
-    end
+    define_models
+    act! { put :update, :id => @user.to_param, :user => {:email => ''}, :format => 'xml' }
     
     it_assigns :user
-    it_renders :xml, "user.errors", :status => :unprocessable_entity
+    it_renders :xml, :status => :unprocessable_entity do
+      assigns(:user).errors.to_xml
+    end
   end
 end
