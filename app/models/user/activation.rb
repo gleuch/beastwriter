09ee60c21ca1ccb1e6c1e789bcbf6afd98c6c2e9
Nick Quaranto <nick@quaran.to>
2008-09-24 01:01:09 -0400

    Sessions are now happy.

--- app/models/user/activation.rb
+++ app/models/user/activation.rb
@@ -1,9 +1,36 @@
 class User
+  include Authentication
+  include Authentication::ByPassword
+  include Authentication::ByCookieToken
+
   after_create :set_first_user_as_activated
   def set_first_user_as_activated
     activate! if site.nil? or site.users.size <= 1
   end
+
+  # Activates the user in the database.
+  def activate!
+    @activated = true
+    self.activated_at = Time.now.utc
+    self.activation_code = nil
+    save(false)
+  end
+
+  # Returns true if the user has just been activated.
+  def recently_activated?
+    @activated
+  end
+
+  def active?
+    # the existence of an activation code means they have not activated yet
+    activation_code.nil?
+  end
+
   
+  
+  
+  
+=begin
   def remember_token?
     active? && remember_token_expires_at && Time.now.utc < remember_token_expires_at 
   end
@@ -28,7 +55,6 @@ class User
     self.remember_token            = nil
     save(false)
   end
-  
   # Activates the user in the database.
   def activate!
     @activated = true
@@ -46,4 +72,6 @@ class User
     # the existence of an activation code means they have not activated yet
     activation_code.nil?
   end
+
+=end
 end
--- spec/controllers/forums_sessions_and_cookies_spec.rb
+++ spec/controllers/forums_sessions_and_cookies_spec.rb
@@ -5,11 +5,14 @@ describe ForumsController, "(remember-me functionality)" do
 
   before do
     session[:forums_page] = {}
+    session[:user_id] = {}
   end
 
   it "logs in with valid login token" do
-    @request.cookies['auth_token'] = CGI::Cookie.new('auth_token', users(:default).remember_token)
+    @request.cookies['auth_token'] = CGI::Cookie.new('auth_token', users(:activated).remember_token)
+    
     get :index
-    controller.send(:current_user).should == users(:default)
+    
+    controller.send(:current_user).should == users(:activated)
   end
 end
