aae0fb3379669bceda0e5267c539507c0e5ddf6d
hamp <hampton@guitree.com>
2008-10-02 20:56:27 -0400

    add more options to user attr_accessible for user settings page to work

--- app/models/user/validation.rb
+++ app/models/user/validation.rb
@@ -17,7 +17,7 @@ class User
 
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :password, :password_confirmation, :bio
+  attr_accessible :login, :email, :password, :password_confirmation, :bio, :website, :display_name, :openid_url 
 
   # Encrypts some data with the salt.
   def self.encrypt(password, salt)
