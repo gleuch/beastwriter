e72cad8d8b48d9e407deba48773a3e23d97fcef3
Ron Damen <jxl@ganja.nl>
2008-12-12 17:10:31 +0100

    Fix deprecation warning for TextHelper::concat
    The binding argument of TextHelper::concat is no longer needed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,6 +67,6 @@ module ApplicationHelper
   end
 
   def for_moderators_of(record, &block)
-    moderator_of?(record) && concat(capture(&block), block.binding)
+    moderator_of?(record) && concat(capture(&block))
   end
 end
